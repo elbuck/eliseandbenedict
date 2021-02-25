f82ac74757a0a4dbb4ebe164e6d85eee0595e867
Tommy Marshall <tmarshall@medallia.com>
2021-02-24 22:04:20 -0500

    fix some window clicking nav toggling bug

--- Scripts/wedding.js
+++ Scripts/wedding.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-// Drop down nav-bar 
+// Drop down nav-bar
 
 const dropDownButton = document.querySelector('.nav-drop-down')
 const navList = document.querySelector('.drop-down-list')
 
 function toggleButton () {
-	navList.classList.toggle('show')
+  navList.classList.toggle('show')
 }
 
 dropDownButton.addEventListener('click', toggleButton)
@@ -14,24 +14,24 @@ dropDownButton.addEventListener('click', toggleButton)
 // Hide drop down nav-bar when click outside of it
 
 window.addEventListener('click', function (event) {
-	if (event.target != dropDownButton && event.target.parentNode != dropDownButton) {
-		navList.style.display = 'none';
-	} 
+  if (event.target != dropDownButton && event.target.parentNode != dropDownButton) {
+    navList.classList.remove('show')
+  }
 });
 
 
 // Countdowwn to wedding date
+const today = new Date ()
 
-	const today = new Date ()
+const Wedding = new Date('2021-10-20T21:58:01.532Z')
 
-	const Wedding = new Date('2021-10-20T21:58:01.532Z')
+const countdown = document.querySelector('.countdown')
 
-	const countdown = document.querySelector('.countdown')
+const millisecondsToWedding = (Wedding - today)
+const days = millisecondsToWedding / 86400000
 
-	const millisecondsToWedding = (Wedding - today)
-	const days = millisecondsToWedding / 86400000
+countdown.innerHTML = Math.round(days)
 
-	countdown.innerHTML = Math.round(days)
 // main page menu sticky
 window.onscroll = function() {myFunction()};
 const navbar = document.getElementById("navbar");
@@ -45,6 +45,6 @@ function myFunction() {
   }
 }
 //
- 
+
 
 })
