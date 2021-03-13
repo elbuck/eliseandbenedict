47a428ca3130a22d37db0f2bea406ffe34763f7f
Tommy Marshall <tmarshall@medallia.com>
2021-03-13 14:57:04 -0500

    add basic RSVP

---
+++ rsvp.php
@@ -0,0 +1,55 @@
+<?php
+
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $visitor_message = "";
+    $email_body = "<div>";
+
+    if(isset($_POST['visitor_name'])) {
+        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                        </div>";
+    }
+
+    if(isset($_POST['visitor_email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+                        </div>";
+    }
+
+    if(isset($_POST['rsvp'])) {
+        $rsvp = filter_var($_POST['rsvp'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>RSVP:</b></label>&nbsp;<span>".$rsvp."</span>
+                        </div>";
+    }
+
+    if(isset($_POST['visitor_message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$visitor_message."</div>
+                        </div>";
+    }
+
+    $email_body .= "</div>";
+
+    $recipient = "elise.buckley@my.avemaria.edu ";
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+
+    if(mail($recipient, $email_title, $email_body, $headers)) {
+        header('Location: /thankyou.html');
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+
+} else {
+    echo '<p>Something went wrong</p>';
+}
